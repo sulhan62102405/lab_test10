1c7f23faa104fad6da5376f8548fa5e375c1441a
sulhan62102405 <sulhansamok@gmail.com>
2022-03-29 09:38:53 +0700

    edit main

--- app/main.py
+++ app/main.py
@@ -12,4 +12,8 @@ def read_root():
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+@app.get("/")
+def read_root():
+    return {"Hello": "SWE"}
+
 handler = Mangum(app)
